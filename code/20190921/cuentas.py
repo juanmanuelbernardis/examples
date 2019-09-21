79c5c63b9bc6d2eb59ca5e00c2b5b9fdc8dfad9c
Juan Manuel Pousadas Bernardis <juanmanuelpb2005@gmail.com>
2019-09-21 17:39:14 -0300

    codigo ejemplo operaciones

---
+++ code/20190921/cuentas.py
@@ -0,0 +1,10 @@
+valor_1 = int(input('Insertar el primer valor: '))
+valor_2 = int(input('Insertar el segundo valor: '))
+print(valor_1 + valor_2)
+print(valor_1 - valor_2)
+print(valor_1 / valor_2)
+print(valor_1 * valor_2)
+print(valor_1 % valor_2)
+
+try:
+    
