459c17c5693760d243e2553e22e3286042626aaf
Alejandro M. Bernardis <alejandro.m.bernardis@gmail.com>
2019-09-21 17:24:23 -0300

    Fixes

--- code/20190921/boletin.py
+++ code/20190921/boletin.py
@@ -1,27 +1,25 @@
 from pathlib import Path
 
+data = {}
 header = None
 filename = Path('data.csv')
 filedata = filename.read_bytes()
-data = {}
 
+# line: "Asignaturas;Trim 1;Trim 2;Trim 3"
 for x in filedata.decode().splitlines():
-    #print(">", x.split(";"))
     line = x.split(';')
-
     if header is None:
         header = line
         continue
-
     data[line[0]] = line[1:]
-    print(line, line[1:])
 
-
+    
 def promedio(trimestre):
     suma = 0
     for k, v in data.items():
         suma += float(v[trimestre-1].replace(',', '.'))
     print('trimestre', trimestre, 'promedio', suma / len(data.keys()))
+
 
 promedio(1)
 promedio(2)
