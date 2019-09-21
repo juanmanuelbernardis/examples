edaeaf0276b14d3bfbd6ca6b396c6c275352e2a3
Juan Manuel Pousadas Bernardis <juanmanuelpb2005@gmail.com>
2019-09-21 18:32:33 -0300

    Create empresa.py

---
+++ code/empresa.py
@@ -0,0 +1,19 @@
+def capturar_valor(mensaje=None):
+    try:
+        return int(input(mensaje or 'Ingrese un valor: '))
+    except:
+        print('El valor es incorrecto')
+        exit()
+
+horas = capturar_valor('Horas trabajadas: ')
+antiguedad = capturar_valor('Años en la empresa: ')
+cobroxhoras = capturar_valor('Cobro por hora: ')
+
+sueldo = horas * cobroxhoras
+
+if antiguedad >= 5:
+    sueldo += 1000
+
+print(sueldo)
+
+PUTO EL QUE LEE
