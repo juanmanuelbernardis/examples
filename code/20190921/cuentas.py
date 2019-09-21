0eb03c932170c4d5fc8e2b1b2516c7be75e50aa4
Juan Manuel Pousadas Bernardis <juanmanuelpb2005@gmail.com>
2019-09-21 17:52:49 -0300

    Update cuentas.py

--- code/20190921/cuentas.py
+++ code/20190921/cuentas.py
@@ -1,10 +1,17 @@
-valor_1 = int(input('Insertar el primer valor: '))
-valor_2 = int(input('Insertar el segundo valor: '))
+
+
+def capturar_valor(mensaje=None):
+    try:
+        return int(input(mensaje or 'Ingrese un valor: '))
+    except:
+        print('El valor es incorrecto')
+        exit()
+
+valor_1 = capturar_valor('Por favor ingrese un valor: ')
+valor_2 = capturar_valor('Por favor ingrese un segundo valor: ')
+
 print(valor_1 + valor_2)
 print(valor_1 - valor_2)
 print(valor_1 / valor_2)
 print(valor_1 * valor_2)
 print(valor_1 % valor_2)
-
-try:
-    
