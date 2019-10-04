fb3d3bdce29cc0de1b96c5cea63c15afa2feb923
Alejandro M. Bernardis <alejandro.m.bernardis@gmail.com>
2019-10-04 00:28:23 -0300

    UPD // Fixes+

--- code/complex/school.py
+++ code/complex/school.py
@@ -1,163 +1,166 @@
 
 # dependencias
+import sys
 import argparse
 from pathlib import Path
-from utils import catch_int, float_helper
+from utils import catch_int, to_float, except_hook
 
 # diccionario em el cual persistiremos los datos cargados desde el archivo.
 data = {}
 
 
 def get_score(clazz, trimester):
-	"""Función que devuelve el valor de una asignatura para un trimestre
-	   determinado."""
-	return data[clazz][trimester-1]
+    """Función que devuelve el valor de una asignatura para un trimestre
+       determinado."""
+    return data[clazz][trimester-1]
 
 
 def get_average(trimester, verbose=False):
-	"""Funcción que cálcula el promedio de un trimestre determinado."""
+    """Funcción que cálcula el promedio de un trimestre determinado."""
 
-	def _verbose(message):
-		"""Función que imprime en pantalla si el modo verbose está activo."""
-		if verbose is True:
-			print(message)
+    def _verbose(message):
+        """Función que imprime en pantalla si el modo verbose está activo."""
+        if verbose is True:
+            print(message)
 
-	# total acumulado de notas.
-	total = 0
+    # total acumulado de notas.
+    total = 0
 
-	# total de asignaturas.
-	total_clazz = len(data.keys())
+    # total de asignaturas.
+    total_clazz = len(data.keys())
 
-	# si el argumento `verbose` es `True`, imprimimos el titulo de las 
-	# asignaturas.
-	_verbose('\nAsignaturas:')
+    # si el argumento `verbose` es `True`, imprimimos el titulo de las 
+    # asignaturas.
+    _verbose('\nAsignaturas:')
 
-	# recorremos las asignatura.
-	for key in data.keys():
-		# calculamos el valor de la asignatura.
-		score = get_score(key, trimester)
+    # recorremos las asignatura.
+    for key in data.keys():
+        # calculamos el valor de la asignatura.
+        score = get_score(key, trimester)
 
-		# si el argumento `verbose` es `True`, imprimimos los datos de la
-		# asignatura.
-		_verbose('- {} {} {}'.format(key, '.' * (32 - len(key)), score))
+        # si el argumento `verbose` es `True`, imprimimos los datos de la
+        # asignatura.
+        _verbose('- {} {} {}'.format(key, '.' * (32 - len(key)), score))
 
-		# sumamos el valor de la asignatura al acumulado.
-		total += score
+        # sumamos el valor de la asignatura al acumulado.
+        total += score
 
-	# si el argumento `verbose` es `True`, imprimimos el valor acumulados de 
-	# las asignaturas.
-	_verbose('--')
-	_verbose(' Total de {} puntos sobre {} materia(s).'
-		     .format(total, total_clazz))
+    # si el argumento `verbose` es `True`, imprimimos el valor acumulados de 
+    # las asignaturas.
+    _verbose('--')
+    _verbose(' Total de {} puntos sobre {} materia(s).'
+             .format(total, total_clazz))
 
-	# calculamos el promedio y lo retornamos.
-	return total / total_clazz
+    # calculamos el promedio y lo retornamos.
+    return total / total_clazz
 
 
 def read_file(filename=None):
-	"""Función que lee el archivo utilizado como base de datos."""
+    """Función que lee el archivo utilizado como base de datos."""
 
-	# cabecera del archivo.
-	header = None
+    # cabecera del archivo.
+    header = None
 
-	# leemos el contenido del archivo en bytes.
-	lines = Path(filename or 'school_data.csv').read_bytes()
+    # leemos el contenido del archivo en bytes.
+    lines = Path(filename or 'school_data.csv').read_bytes()
 
-	# recorremos las líneas del archivos, previa decodificación del mismo de
-	# bytes a string.
-	for line in lines.decode().splitlines():
-		# convertimos el string en una lista, usando el caracter `;` como
-		# separador.
-		line_data = line.split(';')
+    # recorremos las líneas del archivos, previa decodificación del mismo de
+    # bytes a string.
+    for line in lines.decode().splitlines():
+        # convertimos el string en una lista, usando el caracter `;` como
+        # separador.
+        line_data = line.split(';')
 
-		# si la variable `header` es igual a `None`, le asignamos el valor de
-		# la primer línea y continuamos.
-		if header is None:
-			header = line_data
-			continue
+        # si la variable `header` es igual a `None`, le asignamos el valor de
+        # la primer línea y continuamos.
+        if header is None:
+            header = line_data
+            continue
 
-		# agregamos al diccionario las asignaturas, donde la clave es el nombre
-		# de la asignatura y el valor una lista con las notas de cada trimestre.
-		data[line_data[0]] = [float_helper(x) for x in line_data[1:]]
+        # agregamos al diccionario las asignaturas, donde la clave es el nombre
+        # de la asignatura y el valor una lista con las notas de cada trimestre.
+        data[line_data[0]] = [to_float(x) for x in line_data[1:]]
 
 
 def run(args):
-	"""Función que inicia el programa."""
+    """Función que inicia el programa."""
 
-	# verificamos si existe el argumento `trimester`, de no existir solicitamos
-	# el ingreso del mismo.
-	if args.trimester is None:
-		trimester_value = catch_int('Por favor, ingrese el trimestre que '
-									'desea consultar', True)
-	else:
-		trimester_value = args.trimester
+    # verificamos si existe el argumento `trimester`, de no existir solicitamos
+    # el ingreso del mismo.
+    if args.trimester is None:
+        trimester_value = catch_int('Por favor, ingrese el trimestre que '
+                                    'desea consultar', True)
+    else:
+        trimester_value = args.trimester
 
-	# establecemos un limite sobre la cantidad de los trimestres.
-	trimester_value = max(min(trimester_value, 3), 1)
+    # establecemos un limite sobre la cantidad de los trimestres.
+    trimester_value = max(min(trimester_value, 3), 1)
 
-	try:
-		# leemos el archivo `school_data.csv` y lo cargamos dentro de la
-		# variable `data`, la cual se comporta cómo un diccionario.
-		read_file(args.filename)
+    try:
+        # leemos el archivo `school_data.csv` y lo cargamos dentro de la
+        # variable `data`, la cual se comporta cómo un diccionario.
+        read_file(args.filename)
 
-		# calculamos el promedio.
-		average = round(get_average(trimester_value, args.verbose), 2)
+        # calculamos el promedio.
+        average = round(get_average(trimester_value, args.verbose), 2)
 
-		# definimos el mensaje de salida.
-		message = '\n > El promedio del trimestre "{}", es: {} pts.\n'\
-			      .format(trimester_value, average)
+        # definimos el mensaje de salida.
+        message = '\n > El promedio del trimestre "{}", es: {} pts.\n'\
+                  .format(trimester_value, average)
 
-	except Exception as e:
-		# en caso de que se produzca un error, lo capturamos y definimos el
-		# mensaje de salida.
-		message = 'Se produjo un error no controlado: %s' % str(e)
+    except (TypeError, ValueError) as e:
+        # en caso de que se produzca un error, lo capturamos y definimos el
+        # mensaje de salida.
+        message = 'Se produjo un error no controlado: %s' % str(e)
 
-	# imprimimos el mensaje de salida en pantalla.
-	print(message)
+    # imprimimos el mensaje de salida en pantalla.
+    print(message)
 
 
 
 if __name__ == '__main__':
-	"""
-	Algunas formas de ejecutar el programa:
-	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    """
+    Algunas formas de ejecutar el programa:
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-	> python3 school.py -h
-	> python3 school.py -v
-	> python3 school.py -v -t 1
-	> python3 school.py -v -t 1 -f ../data.csv
-	> python3 school.py -t 2 -f ../data.csv
-	> python3 school.py -f ../data.csv
-	"""
+    > python3 school.py -h
+    > python3 school.py -v
+    > python3 school.py -v -t 1
+    > python3 school.py -v -t 1 -f ../data.csv
+    > python3 school.py -t 2 -f ../data.csv
+    > python3 school.py -f ../data.csv
+    """
 
-	# definimos la lista de argumentos que puede recibir el programa.
-	parser = argparse.ArgumentParser()
-	parser.add_argument('trimester', type=str,
-						help='Trimestre a calcular.')
-	parser.add_argument('-f', '--filename', type=str, default=None,
-						help='Archivo a cargar.')
-	parser.add_argument('-v', '--verbose', action='store_true',
-						help='Muestra la lista de asignaturas.')
+    # definimos la lista de argumentos que puede recibir el programa.
+    parser = argparse.ArgumentParser()
+    parser.add_argument('trimester', type=str, help='Trimestre a calcular.')
+    parser.add_argument('-f', '--filename', type=str, default=None,
+                        help='Archivo a cargar.')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='Muestra la lista de asignaturas.')
 
-	# capturamos los argumentos.
-	args = parser.parse_args()
+    # capturamos los argumentos.
+    args = parser.parse_args()
 
-	# verificamos el modo de impresión e iniciamos el programa.
-	if args.trimester == "all":
-		# anulamos el modo `verbose`.
-		args.verbose = False
+    # capturamos los errores no controlados y los imprimimos en pantalla.
+    sys.excepthook = except_hook
 
-		# mostramos todos los trimestres.
-		for t in range(1, 4):
-			# ingresamos el valor del trimeste
-			args.trimester = t
+    # verificamos el modo de impresión e iniciamos el programa.
+    if args.trimester == "all":
+        # anulamos el modo `verbose`.
+        args.verbose = False
 
-			# mostramos el trimestre ingresado.
-			run(args)
-			
-	else:
-		# convertimos el string a un integer
-		args.trimester = int(args.trimester)
+        # mostramos todos los trimestres.
+        for t in range(1, 4):
+            # ingresamos el valor del trimeste
+            args.trimester = t
 
-		# mostramos el trimestre ingresado.
-		run(args)
+            # mostramos el trimestre ingresado.
+            run(args)
+            
+    else:
+        # convertimos el string a un integer
+        args.trimester = int(args.trimester)
+
+        # mostramos el trimestre ingresado.
+        run(args)
--- code/complex/utils.py
+++ code/complex/utils.py
@@ -1,40 +1,65 @@
 from functools import partial
 
 
+def to_int(value):
+    return int(float(value))
+
+
+def to_float(value):
+    return float(value.replace(',', '.'))
+
+
+def to_bool(value):
+    return str(value).lower() in ('true', 'yes', 'si', 'y', 's', '1')
+
+
 def catch_value(message=None, recursive=False, default=None, cast=None):
-	if not isinstance(message, str):
-		message = 'Por favor, ingrese un valor'
-	else:
-		message = message.strip()
-	if not message.endswith(':'):
-		message += ': '
-	if default is not None:
-		message =  '{} [{}]: '.format(message[:-2], str(default))
-	value = input(message).strip()
-	if default is not None and not value:
-		return default
-	if cast is not None:
-		try:
-			value = cast(value)
-		except:
-			if recursive is True:
-				return catch_value(message, recursive, default, cast)
-			raise ValueError('El valor ingresado no es correcto: ', value)
-	return value
+    if not isinstance(message, str):
+        message = 'Por favor, ingrese un valor'
+    else:
+        message = message.strip()
+    if not message.endswith(':'):
+        message += ': '
+    if default is not None:
+        message =  '{} [{}]: '.format(message[:-2], str(default))
+    value = input(message).strip()
+    if default is not None and not value:
+        return default
+    if cast is not None:
+        try:
+            value = cast(value)
+        except:
+            if recursive is True:
+                return catch_value(message, recursive, default, cast)
+            raise ValueError('El valor ingresado no es correcto: ', value)
+    return value
 
 
-def int_helper(value):
-	return int(float_helper(value))
+catch_int = partial(catch_value, cast=to_int)
+catch_float = partial(catch_value, cast=to_float)
+catch_bool = partial(catch_value, cast=to_bool)
 
 
-def float_helper(value):
-	return float(value.replace(',', '.'))
+def tbiter(traceback):
+    while traceback:
+        yield traceback
+        traceback = traceback.tb_next
 
 
-def bool_helper(value):
-	return str(value).lower() in ('true', 'yes', 'si', 'y', 's', '1')
+def tblast(traceback):
+    return [x for x in tbiter(traceback)][-1]
 
 
-catch_int = partial(catch_value, cast=int_helper)
-catch_float = partial(catch_value, cast=float_helper)
-catch_bool = partial(catch_value, cast=bool_helper)
+def except_hook(exception, value, traceback):
+    tb = tblast(traceback)
+    if exception is not KeyboardInterrupt:
+        print('')
+        print('ERROR <{}>:'.format(exception.__name__))
+        print('  > {}'.format(value))
+        data = (tb.tb_frame.f_code.co_filename, tb.tb_lineno)
+        print('  ! File {}, line {}'.format(*data))
+        print('--')
+    else:
+        print('')
+    print('Bye!')
+    exit(-9)
